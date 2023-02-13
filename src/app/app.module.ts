bbc1746b1a97afbbbcc31bfc788119f15d0d5f10
Artemy Golovnev <ae.golovnev@gmail.com>
2023-02-13 22:49:17 +0600

    feat: add fixed sidebar and grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
